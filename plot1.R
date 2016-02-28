638b51867607c0106511eebdc5e3c3d9f791fdbb
Leigh <leighmatthews87@gmail.com>
2016-02-28 17:22:14 -0500

    Add Plot 1 files via upload

---
+++ plot1.R
@@ -0,0 +1,60 @@
+# Coursera
+# Exploratory Data Analysis  --- Assignment 1 -- Plot 1 Histogram
+# Leigh Matthews
+# Feb 28, 2016
+
+
+#### Dataset:  Electronic Power Consumption
+# URL:  https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+
+#### Description: 
+# Measurements of electronic power consumption in one household with 
+# a one-minute sampling rate over a period of almost 4 years.  
+# Different electrical quantities and some sub-metering values are available.
+
+#### Overall Goal: Examine how household energy usage varaies 
+# over a 2-day period in February 2007 by reconstructing the 
+#4 given plots using the base plotting system.
+
+# Remember to set Working Directory and turn off devices
+
+##################################################################################
+
+#### Load the full household power consumption dataset then filter by dates
+
+full_data_to_subset <- function(filepath) {
+
+    # Read in the full data from the filepath parameter
+    x <- read.table(filepath, sep=";", header=TRUE, na.strings="?")
+
+    # Create new "datetime" variable by concatenateing Date and Time (type=POSIXlt)  
+    x$datetime <- strptime(paste(x$Date, x$Time), "%d/%m/%Y %H:%M:%S")
+
+    # Assign the desired dates to subset for the 2-day period [Feb 1, 2007 and Feb 2, 2007]
+    dates <- c(as.Date("2007-02-01"), as.Date("2007-02-02"))
+
+    # Output the subset of data for the 2-day period 
+    x[as.Date(x$datetime) %in% dates,]
+}
+
+######################################################
+
+setwd("C:/Users/Leigh/Documents/GitHub")
+
+#### Create and save Plot 1: red histogram of global active power with x-axis label and title
+
+redplot1 <- function(x) {
+    hist(x$Global_active_power, col="red", 
+    main ="Global Active Power", xlab ="Global Active Power (kilowatts)")
+}
+
+x <- full_data_to_subset("./household_power_consumption.txt")
+
+#### Copy to PNG file and set size
+png("plot1.png", width=480, height=480)
+
+# Run the function to get the histogram in the screen device
+redplot1(x)
+
+#### Turn off device
+dev.off()
